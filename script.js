d92b3086d15e95f361e92aecac9b0e1e753f5380
cyb3rtaye <cyberta7e@gmail.com>
2026-02-24 19:50:37 +0000

    Modify typing effect text and speed
    Updated typing effect speed and text content.

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-// Typing Effect
-const text = "Junior SOC Analyst | Blue Team | Threat Detection Focused";
+const text = "Junior SOC Analyst | Blue Team | Threat Detection";
 let index = 0;
 
 function typeEffect() {
     if (index < text.length) {
         document.querySelector(".typing").textContent += text.charAt(index);
         index++;
-        setTimeout(typeEffect, 40);
+        setTimeout(typeEffect, 50);
     }
 }
 
 window.onload = typeEffect;
-
-// Live Clock
-function updateClock() {
-    const now = new Date();
-    document.getElementById("clock").textContent =
-        "SOC Time: " + now.toLocaleTimeString();
-}
-setInterval(updateClock, 1000);
-
-// Fake Alert Counter
-let alerts = 0;
-setInterval(() => {
-    alerts += Math.floor(Math.random() * 2);
-    document.getElementById("alerts").textContent = alerts;
-}, 3000);
