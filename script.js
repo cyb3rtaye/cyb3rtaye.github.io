ff367d1659ad527c6f9c2bcc877951b498fe1d1a
cyb3rtaye <cyberta7e@gmail.com>
2026-02-24 18:50:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "Junior SOC Analyst | Blue Team | Cybersecurity Enthusiast";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.querySelector(".typing").textContent += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 50);
+    }
+}
+
+window.onload = typeEffect;
